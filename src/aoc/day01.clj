95b62f6021c220371b143f9690c30b0954c11e9b
james <7298563+lamesjinden@users.noreply.github.com>
2022-12-01 00:18:38 -0500

    Add day 1 solution.

---
+++ src/aoc/day01.clj
@@ -0,0 +1,31 @@
+(ns aoc.day01
+  (:require [clojure.string :as str]
+            [aoc.util :as util]))
+
+(def day01-input-path "resources/day01.txt")
+
+(def sample-input "1000\n2000\n3000\n\n4000\n\n5000\n6000\n\n7000\n8000\n9000\n\n10000")
+
+(comment
+
+  (->> (slurp day01-input-path)
+       (util/split-on-newline)
+       (map (fn [ss]
+              (->> ss
+                   (map (fn [s] (Integer/parseInt s)))
+                   (reduce +))))
+       (sort)
+       (last))
+
+  (->> (slurp day01-input-path)
+       (util/split-on-newline)
+       (map (fn [ss]
+              (->> ss
+                   (map (fn [s] (Integer/parseInt s)))
+                   (reduce +))))
+       (sort)
+       (reverse)
+       (take 3)
+       (reduce +))
+
+  )
